577a82aa5dbd64fb600945eb0e64bdfdfc04d065
VadimMustafin <112873237+VadimMustafin@users.noreply.github.com>
2022-09-20 00:12:57 +0300

    Add files via upload

---
+++ PlayerControllerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float intervalTime = 2f;
+    float myTimer;
+
+    void Start()
+    {
+        // Игрок спамит первую собаку в любое время
+        myTimer = intervalTime;
+    }
+
+    
+    void Update()
+    {
+        
+        myTimer += 1 * Time.deltaTime;
+       
+        // Спам собаки если таймер больше названного и нажата кнопка пробел
+        if (Input.GetKeyDown(KeyCode.Space) && myTimer > intervalTime)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            // Сброс таймера
+            myTimer = 0f;
+        }
+
+
+        Debug.Log(myTimer);
+    }
+
+
+}
